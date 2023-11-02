fe5fab66ce346e7fb41a6601171fe51ae276dfa6
Alvaro Barcena <103132738+alvarob27@users.noreply.github.com>
2023-11-02 18:24:01 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,7 @@
+// Create web server
+// Usage: node comments.js
+// Description: This code will create a web server that will listen for requests on port 8080.
+// When a request is received, the server will read the comments.json file and return the contents
+// of the comments.json file to the client.  The comments.json file contains the comments that
+// have been posted.  The comments.json file is created by the post.js code.
+//
